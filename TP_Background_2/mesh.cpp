d7021de4d7219f067476b0d5c8596a7b149aa798
wdwind <wangfenghahaha@gmail.com>
2014-06-18 00:23:40 +0200

    some obj files

--- TP_Background_2/mesh.cpp
+++ TP_Background_2/mesh.cpp
@@ -306,124 +306,130 @@ void Mesh::draw(){
 }
 
 bool Mesh::loadBoss(const char * filename){
-//
-//	std::vector<int> vhandles;
-//
-//	const unsigned int LINE_LEN = 256;
-//	char s[LINE_LEN];
-//	FILE * in;
-//#ifdef WIN32
-//	errno_t error = fopen_s(&in, filename, "r");
-//	if (error != 0)
-//#else
-//	in = fopen(filename, "r");
-//	if (!(in))
-//#endif
-//		return false;
-//
-//	while (in && !feof(in) && fgets(s, LINE_LEN, in))
-//	{
-//		// material file
-//		// vertex
-//		if (strncmp(s, "v ", 2) == 0)
-//		{
-//			if (sscanf_s(s, "v %f %f %f", &x, &y, &z))
-//				vertices.push_back(Vertex(Vec3Df(x, y, z)));
-//		}
-//		// face
-//		else if (strncmp(s, "f ", 2) == 0)
-//		{
-//			int component(0), nV(0);
-//			bool endOfVertex(false);
-//			char *p0, *p1(s + 2); //place behind the "f "
-//
-//			vhandles.clear();
-//
-//			while (*p1 == ' ') ++p1; // skip white-spaces
-//
-//			while (p1)
-//			{
-//				p0 = p1;
-//
-//				// overwrite next separator
-//
-//				// skip '/', '\n', ' ', '\0', '\r' <-- don't forget Windows
-//				while (*p1 != '/' && *p1 != '\r' && *p1 != '\n' &&
-//					*p1 != ' ' && *p1 != '\0')
-//					++p1;
-//
-//				// detect end of vertex
-//				if (*p1 != '/') endOfVertex = true;
-//
-//				// replace separator by '\0'
-//				if (*p1 != '\0')
-//				{
-//					*p1 = '\0';
-//					p1++; // point to next token
-//				}
-//
-//				// detect end of line and break
-//				if (*p1 == '\0' || *p1 == '\n')
-//					p1 = 0;
-//
-//
-//				// read next vertex component
-//				if (*p0 != '\0')
-//				{
-//					switch (component)
-//					{
-//					case 0: // vertex
-//						vhandles.push_back(atoi(p0) - 1);
-//						break;
-//
-//					case 1: // texture coord
-//						//assert(!vhandles.empty());
-//						//assert((unsigned int)(atoi(p0)-1) < texcoords.size());
-//						//_bi.set_texcoord(vhandles.back(), texcoords[atoi(p0)-1]);
-//						break;
-//
-//					case 2: // normal
-//						//assert(!vhandles.empty());
-//						//assert((unsigned int)(atoi(p0)-1) < normals.size());
-//						//_bi.set_normal(vhandles.back(), normals[atoi(p0)-1]);
-//						break;
-//					}
-//				}
-//
-//				++component;
-//
-//				if (endOfVertex)
-//				{
-//					component = 0;
-//					nV++;
-//					endOfVertex = false;
-//				}
-//			}
-//
-//
-//			if (vhandles.size()>3)
-//			{
-//				//model is not triangulated, so let us do this on the fly...
-//				//to have a more uniform mesh, we add randomization
-//				unsigned int k = (false) ? (rand() % vhandles.size()) : 0;
-//				for (unsigned int i = 0; i<vhandles.size() - 2; ++i)
-//				{
-//					triangles.push_back(Triangle(vhandles[(k + 0) % vhandles.size()], vhandles[(k + i + 1) % vhandles.size()], vhandles[(k + i + 2) % vhandles.size()]));
-//				}
-//			}
-//			else if (vhandles.size() == 3)
-//			{
-//				triangles.push_back(Triangle(vhandles[0], vhandles[1], vhandles[2]));
-//			}
-//			else
-//			{
-//				printf("TriMesh::LOAD: Unexpected number of face vertices (<3). Ignoring face");
-//			}
-//		}
-//		memset(&s, 0, LINE_LEN);
-//	}
-//	fclose(in);
-//
+
+	std::vector<int> vhandles;
+
+	const unsigned int LINE_LEN = 256;
+	char s[LINE_LEN];
+	FILE * in;
+#ifdef WIN32
+	errno_t error = fopen_s(&in, filename, "r");
+	if (error != 0)
+#else
+	in = fopen(filename, "r");
+	if (!(in))
+#endif
+		return false;
+
+    float x, y, z;
+
+	while (in && !feof(in) && fgets(s, LINE_LEN, in))
+	{
+		// material file
+		// vertex
+		if (strncmp(s, "v ", 2) == 0)
+		{
+#ifdef WIN32
+			if (sscanf_s(s, "v %f %f %f", &x, &y, &z))
+#else
+			if (sscanf(s, "v %f %f %f", &x, &y, &z))
+#endif
+				vertices.push_back(Vertex(Vec3Df(x, y, z)));
+		}
+		// face
+		else if (strncmp(s, "f ", 2) == 0)
+		{
+			int component(0), nV(0);
+			bool endOfVertex(false);
+			char *p0, *p1(s + 2); //place behind the "f "
+
+			vhandles.clear();
+
+			while (*p1 == ' ') ++p1; // skip white-spaces
+
+			while (p1)
+			{
+				p0 = p1;
+
+				// overwrite next separator
+
+				// skip '/', '\n', ' ', '\0', '\r' <-- don't forget Windows
+				while (*p1 != '/' && *p1 != '\r' && *p1 != '\n' &&
+					*p1 != ' ' && *p1 != '\0')
+					++p1;
+
+				// detect end of vertex
+				if (*p1 != '/') endOfVertex = true;
+
+				// replace separator by '\0'
+				if (*p1 != '\0')
+				{
+					*p1 = '\0';
+					p1++; // point to next token
+				}
+
+				// detect end of line and break
+				if (*p1 == '\0' || *p1 == '\n')
+					p1 = 0;
+
+
+				// read next vertex component
+				if (*p0 != '\0')
+				{
+					switch (component)
+					{
+					case 0: // vertex
+						vhandles.push_back(atoi(p0) - 1);
+						break;
+
+					case 1: // texture coord
+						//assert(!vhandles.empty());
+						//assert((unsigned int)(atoi(p0)-1) < texcoords.size());
+						//_bi.set_texcoord(vhandles.back(), texcoords[atoi(p0)-1]);
+						break;
+
+					case 2: // normal
+						//assert(!vhandles.empty());
+						//assert((unsigned int)(atoi(p0)-1) < normals.size());
+						//_bi.set_normal(vhandles.back(), normals[atoi(p0)-1]);
+						break;
+					}
+				}
+
+				++component;
+
+				if (endOfVertex)
+				{
+					component = 0;
+					nV++;
+					endOfVertex = false;
+				}
+			}
+
+
+			if (vhandles.size()>3)
+			{
+				//model is not triangulated, so let us do this on the fly...
+				//to have a more uniform mesh, we add randomization
+				unsigned int k = (false) ? (rand() % vhandles.size()) : 0;
+				for (unsigned int i = 0; i<vhandles.size() - 2; ++i)
+				{
+					triangles.push_back(Triangle(vhandles[(k + 0) % vhandles.size()], vhandles[(k + i + 1) % vhandles.size()], vhandles[(k + i + 2) % vhandles.size()]));
+				}
+			}
+			else if (vhandles.size() == 3)
+			{
+				triangles.push_back(Triangle(vhandles[0], vhandles[1], vhandles[2]));
+			}
+			else
+			{
+				printf("TriMesh::LOAD: Unexpected number of face vertices (<3). Ignoring face");
+			}
+		}
+		memset(&s, 0, LINE_LEN);
+	}
+	fclose(in);
+
 
 	return 1;
 }
@@ -707,8 +713,8 @@ bool Mesh::loadMesh(int NbVertX, int NbVertY, float qurdSize){
 }
 
 void Mesh::loadRoad(int NbVertX, int NbVertY, float qurdSizeX, float qurdSizeY){
-	cout << qurdSizeX << endl;
-	cout << qurdSizeY << endl;
+	//cout << qurdSizeX << endl;
+	//cout << qurdSizeY << endl;
 
 	//int NbVertX = 7, NbVertY = 2;
 	//float qurdSize = 1;
